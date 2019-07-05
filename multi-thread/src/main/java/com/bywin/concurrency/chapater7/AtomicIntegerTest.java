26f7974fbadde17c2018ddfc0835430a29d06d41
yerikm@163.com <yerikm@163.com>
2019-07-05 17:28:06 +0800

    atomicInteger原子类

---
+++ multi-thread/src/main/java/com/bywin/concurrency/chapater7/AtomicIntegerTest.java
@@ -0,0 +1,43 @@
+package com.bywin.concurrency.chapater7;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * AtomicInteger提供原子操作来进行Integer的使用
+ * created by yeric on 2019/7/5
+ */
+public class AtomicIntegerTest {
+    private static final int THREADS_CONUT = 20;
+//    public static volatile int count = 0;
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    public static void increase() {
+//        count++;
+        count.incrementAndGet();
+    }
+
+    /**
+     * 需要使用debug模式启动
+     * @param args
+     */
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_CONUT];
+        for (int i = 0; i < THREADS_CONUT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 1000; i++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+        System.out.println(count);
+    }
+}
