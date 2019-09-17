ea4239710a7fed68a22b356081eb714154f5d538
yerikm@163.com <yerikm@163.com>
2019-09-17 15:01:38 +0800

    execu工具类中加入json字符串格式化输出

--- multi-thread/src/main/java/com/bywin/util/ExcelUtils.java
+++ multi-thread/src/main/java/com/bywin/util/ExcelUtils.java
@@ -65,6 +65,20 @@ public class ExcelUtils {
     }
 
     /**
+     * 单元格左对齐
+     *
+     * @param workbook
+     * @return
+     */
+    public static CellStyle getLeftStyle(Workbook workbook) {
+        CellStyle leftStyle = workbook.createCellStyle();
+        leftStyle.setAlignment(XSSFCellStyle.ALIGN_LEFT);
+        leftStyle.setVerticalAlignment(XSSFCellStyle.VERTICAL_CENTER);
+        leftStyle.setWrapText(true);
+        return leftStyle;
+    }
+
+    /**
      * 执行导出
      *
      * @param workbook
@@ -81,26 +95,90 @@ public class ExcelUtils {
         }
     }
 
-    public static String null2String (Object obj) {
+    public static String null2String(Object obj) {
         return obj != null ? String.valueOf(obj) : "";
     }
 
     /**
      * 将日期转化为String
+     *
      * @param data
      * @param format
      * @return
      */
     public static String date2String(Date data, String... format) {
-        if(data == null){
+        if (data == null) {
             return "";
         }
         SimpleDateFormat sdf = null;
-        if(format == null || format.length == 0){
+        if (format == null || format.length == 0) {
             sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        }else{
+        } else {
             sdf = new SimpleDateFormat(format[0]);
         }
         return sdf.format(data);
+    }
+
+    /**
+     * json格式化
+     *
+     * @param jsonStr
+     * @return
+     */
+    public static String formatJson(String jsonStr) {
+        if (null == jsonStr || "".equals(jsonStr)) return "";
+        StringBuilder sb = new StringBuilder();
+        char last = '\0';
+        char current = '\0';
+        int indent = 0;
+        for (int i = 0; i < jsonStr.length(); i++) {
+            last = current;
+            current = jsonStr.charAt(i);
+
+            //遇到{[换行，且下一行缩进
+            switch (current) {
+                case '{':
+                case '[':
+                    sb.append(current);
+                    sb.append('\n');
+                    indent++;
+                    addIndentBlank(sb, indent);
+                    break;
+
+                //遇到}]换行，当前行缩进
+                case '}':
+                case ']':
+                    sb.append('\n');
+                    indent--;
+                    addIndentBlank(sb, indent);
+                    sb.append(current);
+                    break;
+
+                //遇到,换行
+                case ',':
+                    sb.append(current);
+                    if (last != '\\') {
+                        sb.append('\n');
+                        addIndentBlank(sb, indent);
+                    }
+                    break;
+                default:
+                    sb.append(current);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 添加space
+     *
+     * @param sb
+     * @param indent
+     */
+    private static void addIndentBlank(StringBuilder sb, int indent) {
+        for (int i = 0; i < indent; i++) {
+            sb.append('\t');
+        }
     }
 }
