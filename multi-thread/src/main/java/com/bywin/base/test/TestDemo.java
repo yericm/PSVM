419dba8b7c59ed8563625a55a91192b00f275abf
yerikm@163.com <yerikm@163.com>
2019-09-26 16:02:29 +0800

    commit

--- multi-thread/src/main/java/com/bywin/base/test/TestDemo.java
+++ multi-thread/src/main/java/com/bywin/base/test/TestDemo.java
@@ -12,32 +12,6 @@ import java.util.List;
 public class TestDemo {
 
     public static void main(String[] args) throws IOException {
-/*        HashSet<Person> set = new HashSet<Person>();
-        set.add(new Person(110,"狗娃"));
-        set.add(new Person(110,"狗娃1"));
-        System.out.println("集合的元素："+ set);*/
-
-        List<String> list = new ArrayList<String>();
-        list.add("张三1");
-        list.add("张三2");
-        list.add("张三3");
-        list.add("张三4");
-        list.add("张三5");
-        list.add("张三6");
-
-        //使用迭代器遍历ArrayList集合
-        Iterator<String> listIt = list.iterator();
-        while (listIt.hasNext()) {
-            Object obj = listIt.next();
-            System.out.println("remove 前：" + obj);
-            if (obj.equals("张三3")) {
-//                list.remove(obj);
-                listIt.remove();
-            }
-        }
-        for (String l : list) {
-            System.out.println(l);
-        }
 
     }
 }
